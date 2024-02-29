34dad694110abe540f0645f62b3f11f8ad6fb18b
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-03-01 00:01:42 +0500

    Add files via upload

---
+++ 11.Names.ts
@@ -0,0 +1,9 @@
+import { Console } from "console";
+
+let names : string [] = ['Arshad','Saim','Sarim','Umar','Alisha','Alishba'];
+
+for (let i = 0; i <names .length; i++) {
+    
+    console.log(names[i]);
+    
+}
