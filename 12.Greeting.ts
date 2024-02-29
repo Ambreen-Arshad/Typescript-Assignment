dc207b4cb976d5fce6f44708197508d105fe2828
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-03-01 00:06:25 +0500

    Add files via upload

---
+++ 12.Greeting.ts
@@ -0,0 +1,11 @@
+import { Console } from "console";
+
+let names : string [] = ['Arshad','Saim','Sarim','Umar','Alisha','Alishba'];
+
+const message : string = 'Welcome to Typesdript class : '
+
+for (let i = 0; i <names .length; i++) {
+    
+    console.log( message + names[i]);
+    
+}
