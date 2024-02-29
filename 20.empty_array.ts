392cfe117ae2a1daa15b985f11b999c924c3b366
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-03-01 00:29:24 +0500

    Add files via upload

---
+++ 20.empty_array.ts
@@ -0,0 +1,20 @@
+// Date : 26-2-2024
+// This is "Ambreen" here,and you are watching the solution of the problem no : 20 in typescript.
+
+
+// Think of something you could store in a array. For example,\n
+// you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like.\n
+// Write a program that creates a list containing these items.
+
+let items : string[] = ['Pakistan', 'Karachi','Urdu'];
+
+// Write a program that creates a list containing these items.
+
+let items2 : string[] = [];
+
+items2.push('Germany');
+items2.push('England');
+items2.push('India');
+
+console.log(items2);
+
