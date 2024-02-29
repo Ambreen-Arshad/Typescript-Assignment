2acfb8d47780cf675365f7d9e9359b1a06c9ae87
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-03-01 00:22:34 +0500

    Add files via upload

---
+++ 18.Seeing_the_World.ts
@@ -0,0 +1,32 @@
+// Date : 21-2-2024
+// This is "Ambreen" here,and you are watching the solution of the problem no: 18 in typescript assignment.
+// Seeing the World: Think of at least five places in the world you’d like to visit.
+
+//• Store the locations in a array. Make sure the array is not in alphabetical order.
+let places : string [] = ['Khan-e-Kaba`a','Burj Khalifa','Nagera Falls','Eifel Tower','Italy'];
+
+// • Print your array in its original order.
+console.log('orignal '+ places );
+// • Print your array in alphabetical order without modifying the actual list.
+console.log( 'copy '+ [...places].sort());
+// • Show that your array is still in its original order by printing it.
+console.log('orignal '+ places );
+// • Print your array in reverse alphabetical order without changing the order of the original list.
+console.log( 'copy '+[...places].sort().reverse());
+// • Show that your array is still in its original order by printing it again.
+console.log('orignal '+ places );
+// • Reverse the order of your list. Print the array to show that its order has changed.
+console.log('orignal '+ places.reverse() );
+// • Reverse the order of your list again. Print the list to show it’s back to its original order.
+console.log('orignal '+ places.reverse()); 
+// • Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+console.log('orignal '+ places.sort());
+// • Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+console.log('orignal '+ places.sort().reverse()); 
+
+
+
+
+
+
+
