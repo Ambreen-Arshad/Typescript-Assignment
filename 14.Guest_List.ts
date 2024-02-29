5167802078f0a60890036aa90833670c6d5d3014
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-03-01 00:09:34 +0500

    Add files via upload

---
+++ 14.Guest_List.ts
@@ -0,0 +1,12 @@
+// Date : 18-2-2024
+// This is Ambreen,and you are watching the solution of the problems in Typescript:
+// Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite?\n
+// Make a list that includes at least three people you’d like to invite to dinner.\n
+// Then use your list to print a message to each person, inviting them to dinner.
+
+let Guest_list : string[] = ['Quaid-e-Azam','Alama_Iqbal','Sir_Syed_Ahamad_Khan',]; 
+
+for (let i = 0; i < Guest_list .length; i++) {
+
+    console.log ('Dear Mr. ' +Guest_list [i] + `,\n\n It is our pleasure to invite you in our party.\n\n Thank You!`)
+}
