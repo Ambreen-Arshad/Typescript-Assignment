93f855a75168a7d5422c42a05ba82dfffed1d9d1
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-02-29 23:44:43 +0500

    Add files via upload

---
+++ 6.Stripping_Names.ts
@@ -0,0 +1,9 @@
+const person_Name : string = '    \n\tArshd Kalam\t\n    ';
+
+console.log(person_Name);
+
+const without_whitespace : string = person_Name.trim();
+
+console.log(without_whitespace);
+
+
