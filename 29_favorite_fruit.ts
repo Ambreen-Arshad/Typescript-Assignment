50be9d557e5c0f60cbd19c5204ef526329c3d6d3
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-03-07 01:29:04 +0500

    Add files via upload

---
+++ 29_favorite_fruit.ts
@@ -0,0 +1,29 @@
+// Date 5-3-2024
+// This is "Ambreen",and you are watching the solution of the problem on 29 in typescript.
+
+// Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that \n
+//check for certain fruits in your array.
+//• Make a array of your three favorite fruits and call it favorite_fruits.
+//• Write five if statements. Each should check whether a certain kind of fruit is in your array.\n
+// If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+
+
+let favoriteFruit : string[] = ['Mango','Banana','Grapes'];
+if ( favoriteFruit.includes('Mango')){
+    console.log("I really like Mango");
+}
+if( favoriteFruit.includes('Banana')){
+    console.log("I really like Banana");
+}
+if( favoriteFruit.includes('Grapes')){
+    console.log("I really like Grapes");
+}
+if( favoriteFruit.includes('Apple')){
+    console.log("I really like Apple");
+}
+if( favoriteFruit.includes('Orange')){
+    console.log("I really like Orange");
+}
+
+
+
